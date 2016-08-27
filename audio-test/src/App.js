132419a56da7a9b5036f22de8a4ed0101a604f02
Ivan Trujillo <krival1987@gmail.com>
2016-08-27 16:01:40 -0400

    Oscillator done, we can make sounds with the keyboard, each key is assigned a specific value. Sound can be modified via key press, it does not overload the AudioCtx, instead it is a continous song. We are on our way! Joe and I be killing it

--- audio-test/src/App.js
+++ audio-test/src/App.js
@@ -3,39 +3,155 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+let audioCtx = new window.AudioContext();
+let oscillator = audioCtx.createOscillator();
+oscillator.connect(audioCtx.destination);
+oscillator.frequency.value = 0;
+oscillator.start()
+console.log(audioCtx);
+
 class App extends Component {
   constructor(props){
     super(props)
     this.state = {
-      a: 440,
-      c: 261.6,
-      term: ""
+      //white keys frequency
+      e3: 164.814,//a
+      f3: 174.614,//s
+      g3: 195.998,//d
+      a3: 220,//f
+      b3: 246.942,//g
+      c4: 261.626,//h
+      d4: 293.665, //j
+      e4: 329.628, //k
+      f4: 349.228, //l
+      g4: 391.995, //;
+
+      //black keys frequency
+      ds3: 155.563, //q
+      fs3: 184.997, //e
+      gs3: 207.652, //r
+      as3: 233.082, //t
+      cs4: 277.183, //u
+      ds4: 311.127,//i
+      fs4: 369.994,//o
+      gs4: 415.305,//p
+      as4: 466.164,//[
+      sound: ""
     }
   }
-  move(e){
-    console.log('this is', setState({term: e.target.value}))
-  }
-  handleSound(note){
-    console.log("starting up")
-    note = this.state.term
-if(note === this.state.a){
-  return note
 
-}else if (note === this.state.c) {
-  return note
-}console.log("this is", note)
-    let audioCtx = new window.AudioContext();
-    let oscillator = audioCtx.createOscillator();
-    oscillator.type = 'square'
+  componentDidMount(){
+    window.addEventListener('keydown', this.handleKeyDown.bind(this), false);
+    window.addEventListener('keyup', this.handleKeyUp.bind(this), false);
+  };
+
+   handleSound(note){
+
+    //let oscillator = audioCtx.createOscillator();
+
     oscillator.frequency.value = note
     oscillator.connect(audioCtx.destination);
+  }
 
-    oscillator.start()
-    oscillator.stop(2)
+  handleKeyUp(event) {
+    console.log("hi");
+    oscillator.frequency.value = 0;
+  }
+
+     handleKeyDown(event){
+/* This will handle frequency conversion that will be passed to handle sound*/
+
+       console.log("I have been pressed", event.keyCode)
+       console.log("starting up")
+       let note;
+       if(event.keyCode === 65){
+         note = this.state.e3
+         console.log(note);
+       }else if(event.keyCode === 83){
+         note = this.state.f3
+
+       }else if (event.keyCode === 68) {
+         //oscillator.stop()
+         note = this.state.g3
+
+       }else if (event.keyCode === 70) {
+         note = this.state.a3
+
+       }else if (event.keyCode === 71) {
+         note = this.state.b3
+
+       }else if (event.keyCode === 72) {
+         note = this.state.c4
+
+       }else if (event.keyCode === 74) {
+         note = this.state.d4
+
+       }else if (event.keyCode === 75) {
+         note = this.state.e4
+
+       }else if (event.keyCode === 76) {
+         note = this.state.f4
+
+       }else if (event.keyCode === 186) {
+         note = this.state.g4
+
+       }/* These are the black key frequencies*/
+       else if (event.keyCode === 81) {
+         note = this.state.ds4
+
+       }else if (event.keyCode === 69) {
+         note = this.state.fs3
+
+       }else if (event.keyCode === 82) {
+         note = this.state.gs3
+
+       }else if (event.keyCode === 74) {
+         note = this.state.as3
+
+       }else if (event.keyCode === 85) {
+         note = this.state.cs4
+
+       }else if (event.keyCode === 73) {
+         note = this.state.ds4
+
+       }else if (event.keyCode === 79) {
+         note = this.state.fs4
+
+       }else if (event.keyCode === 80) {
+         note = this.state.gs4
+
+       }else if (event.keyCode === 219) {
+         note = this.state.as4
+
+     }
+     this.handleSound(note)
+   }
+  render() {
+    return (
+
+        <div className="App" onKeyDown={(()=> this.handleKeyDown())} >
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+          <button onClick={(() => this.handleSound())}>Beep</button>
+
+        </p>
+
+      </div>
+    );
+  }
 }
+
+export default App;
+
+
 //   handleSound(){
 //     console.log("starting up")
-//
+ //
 //     var audio = new window.webkitAudioContext(),
 //         position = 0,
 //         scale = {
@@ -43,26 +159,27 @@ if(note === this.state.a){
 //             f: 349.23,
 //             e: 329.63,
 //             b: 493.88,
-// c: 261.6, a: 440
+//             c: 261.6,
+//             a: 440
 //         },
 //         song = "cfefcc-fff-abb-=afefaaccffgfe---";
-//
+ //
 //     setInterval(play, 1000 / 4);
-//
+ //
 //     function createOscillator(freq) {
 //         var osc = audio.createOscillator();
-//
+ //
 //         osc.frequency.value = freq;
 //         osc.type = "square";
 //         osc.connect(audio.destination);
 //         osc.start(0);
-//
+ //
 //         setTimeout(function() {
 //             osc.stop(0);
 //             osc.disconnect(audio.destination);
 //         }, 1000 / 4)
 //     }
-//
+ //
 //     function play() {
 //         var note = song.charAt(position),
 //             freq = scale[note];
@@ -74,44 +191,4 @@ if(note === this.state.a){
 //             createOscillator(freq);
 //         }
 //     }
-    // oscillator.start()
-    // oscillator.stop(.455)
 //  }
-playSpecificNote(e) { //todo
-		// onKeyDown={this.playSpecificNote}
-		// onKeyUp={this.stopSpecificNote}
-
-		//todo: get focus
-		let keyPressed = e;
-
-		console.log("e", e);
-
-		// if (isFunctionalKey(keyPressed)) {
-		// 	PianoActions.playSpecificNote(this.props, keyPressed);
-		// }
-	}
-
-  render() {
-    let note = this.setState.term
-    return (
-      // <div className="App" onKeyDown={(e) => this.setState({note)} >
-        <div className="App" onKeyDown={(e) => this.move()} >
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-          <button>Beep</button>
-
-        </p>
-        <div className="keyboard">
-
-        </div>
-      </div>
-    );
-  }
-}
-
-export default App;
