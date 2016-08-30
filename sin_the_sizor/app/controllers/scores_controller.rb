43472d6d08612b239b7dc1ab95a30daa37f804d5
joseph sul <joesul7@gmail.com>
2016-08-29 21:20:05 -0400

    changes

--- sin_the_sizor/app/controllers/scores_controller.rb
+++ sin_the_sizor/app/controllers/scores_controller.rb
@@ -11,4 +11,21 @@ class ScoresController < ApplicationController
     render json: scores
   end
 
+  def create
+    puts "params for create", params[:user], params[:score]
+    new_score = Score.new(user: params[:user], score: params[:score])
+
+    if new_score.save
+      render json: new_score
+    else
+      render json: {'error': new_score.errors}
+    end
+  end
+
+  def destroy
+   found_user = Score.where(user: params[:user])
+   Score.destroy(found_user)
+     render json: {'users': Score.all}
+ end
+
 end
--- sin_the_sizor/config/routes.rb
+++ sin_the_sizor/config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   # active record db to handle scores
   get "/scores" => "scores#index"
   get "/scores/:user" => "scores#show"
-
+  post "/scores/new" => "scores#create"
+  delete "/scores/:user" => "scores#destroy"
+  
 end
